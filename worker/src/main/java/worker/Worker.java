db3afa28646d9842257219923ead1fc0e68bbaa6
Ruslan Mustaev <mustaev.ruslan@hotmail.com>
2021-01-19 15:28:05 +0300

    Fix postgres login and password

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -73,7 +73,7 @@ class Worker {
 
       while (conn == null) {
         try {
-          conn = DriverManager.getConnection(url, "postgres", "postgres");
+          conn = DriverManager.getConnection(url, "postgres_user", "postgres_password");
         } catch (SQLException e) {
           System.err.println("Waiting for db");
           sleep(1000);
